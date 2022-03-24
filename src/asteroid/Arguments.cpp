0bf3c1fbad10ea4e15529f7f979a08b465dd8634
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2022-03-24 09:29:51 +0100

    Help message

--- src/asteroid/Arguments.cpp
+++ src/asteroid/Arguments.cpp
@@ -47,7 +47,15 @@ Arguments::Arguments(int argc, char * argv[]):
 
 void Arguments::printHelp() 
 {
-  Logger::info << "Help message" << std::endl;
+  Logger::info << "Arguments:" << std::endl;
+  Logger::info << "-i | --input-gene-trees <STRING>      \t Path to file containing one gene tree per line, in newick format" << std::endl;
+  Logger::info << "-r | --random-starting-trees <INTEGER>\t Number of starting random trees. 0 to start from an ASTRID tree. Default value is 1." << std::endl;
+  Logger::info << "-b | --bs-trees <INTEGER>             \t Number of bootstrap trees to compute. Default is 0." << std::endl;
+  Logger::info << "-p | --prefix <STRING>                \t Prefix for the output files." << std::endl;
+  Logger::info << "-m | --gene-species-mapping <STRING>  \t Path to the mapping file. If unset, Asteroid assumes that the gene tree leaf labels correspond to the species names. See our wiki for the format." << std::endl;
+  Logger::info << "--seed <INTEGER>                      \t Random seed. Default is 1." << std::endl;
+  Logger::info << "-n | --no-correction                  \t When set, Asteroid disables its missing data correction, and the algorithm is then equivalent to ASTRID." << std::endl;
+  Logger::info << "--min-bl <REAL>                       \t Branches with a length under this value will be contracted into polotomies before computing the distance matrices. DEfault value is -1.0 (no contraction)" << std::endl;
 }
 
 void Arguments::printCommand() {
